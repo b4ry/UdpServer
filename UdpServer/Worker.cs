53128644fc78136802645ed70f768680e83e8921
b4ry <barry90@interia.pl>
2024-05-29 20:57:38 +0200

    - move while loop a bit deeper, so we can have a finally clause

--- UdpServer/Worker.cs
+++ UdpServer/Worker.cs
@@ -6,7 +6,7 @@ namespace UdpServer
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
-        private readonly UdpClient _udpServer;
+        private readonly UdpClient _server;
         private readonly IReceivedMessageProcessor _receivedMessageProcessor;
         private readonly int _port;
 
@@ -14,7 +14,7 @@ namespace UdpServer
         {
             _logger = logger;
             _port = int.Parse(Environment.GetEnvironmentVariable("UDP_PORT") ?? "11000");
-            _udpServer = new UdpClient(_port);
+            _server = new UdpClient(_port);
             _receivedMessageProcessor = receivedMessageProcessor;
         }
 
@@ -26,29 +26,29 @@ namespace UdpServer
             {
                 _logger.LogInformation("Cancellation requested.\nCleaning up the resources.");
 
-                _udpServer.Close();
+                _server.Close();
             });
 
-            while (!stoppingToken.IsCancellationRequested)
+            try
             {
-                try
+                while (!stoppingToken.IsCancellationRequested)
                 {
-                    if (_udpServer.Available > 0) // only read when there is some data available
+                    if (_server.Available > 0) // only read when there is some data available
                     {
-                        var receivedData = await _udpServer.ReceiveAsync(stoppingToken);
+                        var receivedData = await _server.ReceiveAsync(stoppingToken);
                         var decodedReceivedData = _receivedMessageProcessor.DecodeReceivedMessage(receivedData);
 
                         await Task.Delay(1000, stoppingToken); // we need this to save a bit of CPU time. Otherwise this loop will go crazy
                     }
                 }
-                catch(Exception ex)
-                {
-                    _logger.LogError(ex, "An exception occured in the server");
-                }
-                finally
-                {
-                    _udpServer.Close();
-                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An exception occured in the server");
+            }
+            finally
+            {
+                _server.Close();
             }
 
             _logger.LogInformation("Server got stopped.");
